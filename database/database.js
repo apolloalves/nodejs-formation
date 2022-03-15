2c6d133c39c616ce079aceb111ccf821556595c7
APOLLO ALVES <apolloapolloalves@gmail.com>
2022-03-15 16:31:20 -0300

    Quiz with NodeJS

---
+++ database/database.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize')
+
+const connection = new Sequelize('guidequiz', 'apollo__nicolly', 'bash', {
+
+    host: 'localhost', 
+    dialect: 'mysql'
+})
+
+
+
+module.exports = connection
---
+++ index.js
@@ -0,0 +1,38 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+const connection = require('./database/database')
+
+//Promisse
+connection
+    .authenticate()
+    .then(() => {
+        console.log('Connection on database')
+    })
+    .catch((err) => console.log( err ))
+
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+
+app.get('/', ( req, res ) => { res.render('index') })
+app.get('/question', ( req, res ) => { res.render('question')})
+
+app.post('/savequestion', ( req, res ) => {
+
+    const data = {
+        title: req.body.title, 
+        description: req.body.description
+    }
+    
+
+    res.send(`Data received : ${data.title} -> ${data.description}`)
+})
+
+
+app.listen(3000, () => console.log('Server is running at http://localhost:3000'))
